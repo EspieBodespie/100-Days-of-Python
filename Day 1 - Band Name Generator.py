de10ebed8b63899ca13c2d42274b46e45ae6c191
EspieBodespie <51254988+EspieBodespie@users.noreply.github.com>
2023-01-12 19:52:30 -0700

    Add files via upload

---
+++ Day 1 - Band Name Generator.py
@@ -0,0 +1,4 @@
+print("This is the greeting for my Band Name Generator Program")
+city = input("What city did you grow up in?\n")
+name = input("What is the name of your pet?\n")
+print("Your band name is " + city + " " + name)
